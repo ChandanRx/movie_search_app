1b39e469e0c5128bb0751e0a57876315f087570c
Chandan rxn <107489996+ChandanRx@users.noreply.github.com>
2024-03-19 13:57:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='movie/:id' element={<SingleMovie/>} />
-          <Route path='*' element={<Error/>} />
+         // <Route path='*' element={<Error/>} />
         </Routes>
     
     </>
